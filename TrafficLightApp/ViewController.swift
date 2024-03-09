6d30048a6b2d4ddefb3bc01daa0d42ce8061007c
OSIIOSdev <OSIIOS.dev@gmail.com>
2024-03-09 13:03:29 -0800

    1. UI elements added and positioned

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
